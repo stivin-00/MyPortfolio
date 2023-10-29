bd7739f3fce5ca5b21af5d1a27d7b56834836fe0
stivin-00 <68236676+stivin-00@users.noreply.github.com>
2023-10-29 18:56:33 +0100

    new edits

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ import backend from "../public/backend.jpg";
 import web from "../public/web.jpg";
 import mobile from "../public/mobile.jpg";
 import Image from "next/image";
-import web1 from "../public/port1.png";
-import web2 from "../public/port2.png";
-import web3 from "../public/port3.png";
+import web1 from "../public/web1.jpeg";
+import web2 from "../public/web2.jpeg";
+import web3 from "../public/web3.jpeg";
 import web4 from "../public/port4.jpg";
 import web5 from "../public/port5.jpg";
 import web6 from "../public/icon.png";
@@ -107,16 +107,13 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              I'm a dedicated and passionate frontend developer with a strong
+              skill set in TypeScript, React, Next.js, and Node.js. My
+              experience includes building, deploying, and managing full-stack
+              applications, as exemplified by my work at balm.ai. Additionally,
+              I have a proven ability to integrate server APIs seamlessly and
+              collaborate effectively within cross-functional teams, which I
+              showcased during my tenure at Bookishmate.com.
             </p>
           </div>
           <div className="lg:flex gap-10">
@@ -170,37 +167,22 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              My professional journey is a testament to my commitment to
+              excellence and my unwavering passion for front-end development. I
+              am excited about the potential to contribute my skills and
+              expertise to new and exciting projects. As I continue to evolve as
+              a developer, I look forward to taking on fresh challenges and
+              creating solutions that have a real impact.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              Feel free to explore my portfolio to see examples of my work and
+              get in touch if you'd like to discuss potential collaborations or
+              opportunities.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
-              <a target="_blank" href="https://app.balm.ai/" rel="noreferrer">
-                <Image
-                  className="rounded-lg object-cover"
-                  width={"100%"}
-                  height={"100%"}
-                  layout="responsive"
-                  src={web8}
-                  alt="pic"
-                />
-              </a>
-            </div>
-            <div className="basis-1/3 flex-1 ">
-              <a
-                target="_blank"
-                href="https://jumstore.netlify.app/"
-                rel="noreferrer"
-              >
+              <a target="_blank" href="https://balm.ai/" rel="noreferrer">
                 <Image
                   className="rounded-lg object-cover"
                   width={"100%"}
@@ -211,10 +193,10 @@ export default function Home() {
                 />
               </a>
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 ">
               <a
                 target="_blank"
-                href="https://eflixx.netlify.app/movies"
+                href="https://bookishmate.com/"
                 rel="noreferrer"
               >
                 <Image
@@ -228,11 +210,7 @@ export default function Home() {
               </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <a
-                target="_blank"
-                href="https://decoooo.netlify.app/"
-                rel="noreferrer"
-              >
+              <a target="_blank" href="https://martiful.com" rel="noreferrer">
                 <Image
                   className="rounded-lg object-cover"
                   width={"100%"}
@@ -243,18 +221,7 @@ export default function Home() {
                 />
               </a>
             </div>
-            <div className="basis-1/3 flex-1">
-              <a target="_blank" href="https://" rel="noreferrer">
-                <Image
-                  className="rounded-lg object-cover"
-                  width={"100%"}
-                  height={"100%"}
-                  layout="responsive"
-                  src={web4}
-                  alt="pic"
-                />
-              </a>
-            </div>
+
             <div className="basis-1/3 flex-1">
               <a
                 target="_blank"
@@ -272,6 +239,10 @@ export default function Home() {
               </a>
             </div>
           </div>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            Thank you for visiting my portfolio, and I'm excited to connect with
+            you.
+          </p>
         </section>
         <footer className="text-center text-teal-500">
           Copyrigth ©2023 All rights reserved | powered by stivin
